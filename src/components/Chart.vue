e21c9db995e922a5ffea718e4637842bf4ef5829
13632301644 <29115361@qq.com>
2018-07-24 18:06:06 +0800

    vue学习工程

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="chartElem">
     <h2>Chart test!</h2>
-    <div class="row">
-      <highcharts class="chart" :options="chartOptions" :updateArgs="updateArgs"></highcharts>
+    <div class="row" style="border: 1px solid rgb(204, 204, 204); position: relative; margin: 5px 0px; text-align: center;">
+      <highcharts class="chart" :options="uriCount" :updateArgs="updateArgs"></highcharts>
+    </div>
+    <div class="row" style="border: 1px solid rgb(204, 204, 204); position: relative; margin: 5px 0px; text-align: center;">
+      <highcharts class="chart" :options="uriLatencySum" :updateArgs="updateArgs"></highcharts>
     </div>
   </div>
 </template>
@@ -10,60 +13,12 @@
 <script>
 export default {
   data () {
-    return {
+    let data = {
       updateArgs: [true, true, {duration: 1000}],
-      // chartOptions: {}
-      chartOptions: {
-        'chart': {
-          'renderTo': 'req_timeoutCount_chart_all',
-          'type': 'spline',
-          'zoomType': 'x',
-          'panning': true,
-          'panKey': 'shift'
-        },
-        'credits': {
-          'enabled': false
-        },
-        'title': {
-          'useHTML': true,
-          'text': '请求超时数',
-          'style': {
-            'font': 'normal 16px Verdana, sans-serif'
-          }
-        },
-        'xAxis': {
-          'type': 'datetime',
-          'labels': {}
-        },
-        'yAxis': {
-          'title': {
-            'text': '次数'
-          },
-          'labels': {}
-        },
-        'plotOptions': {
-          'spline': {
-            'marker': {
-              'enabled': false
-            },
-            'turboThreshold': 5000
-          },
-          'series': {
-            'cursor': 'pointer',
-            'events': {}
-          }
-        },
-        'tooltip': {
-          'crosshairs': true,
-          'shared': true
-        },
-        'exporting': {
-          'enabled': false
-        },
-        'series': [
-        ]
-      }
+      uriCount: this.buildChartOptions('请求数'),
+      uriLatencySum: this.buildChartOptions('请求总耗时')
     }
+    return data
   },
   mounted () {
     this.init()
@@ -73,24 +28,93 @@ export default {
   },
   methods: {
     init () {
-      let url = 'http://localhost:8087/api/query?service=mobAttention&metric=uri.count&startTime=1530523800&endTime=1530529200&aggregator=sum&downsample=1m-sum&uri=3110_1'
-      this.$http.get(url).then((resp) => {
+      let params = {
+        metric: '',
+        service: 'mobAttention',
+        startTime: 1530523800,
+        endTime: 1530529200,
+        aggregator: 'zimsum',
+        downsample: '1m-sum',
+        uri: '3110_1'
+      }
+      params.metric = 'uri.count'
+      this.renderChart({params: params}, this.uriCount)
+      params.metric = 'uri.latency.sum'
+      this.renderChart({params: params}, this.uriLatencySum)
+    },
+    renderChart (params, chart) {
+      // let url = 'http://localhost:8087/api/query'
+      let url = 'http://'+window.location.hostname+':8087/api/query'
+      this.$http.get(url, params).then((resp) => {
         // get body data
         console.info(resp)
         let series = this.convert(resp.body)
         console.info(series)
-        this.chartOptions.series = series
+        chart.series = series
       }, (resp) => {
         console.error(JSON.stringify(resp))
         // error callback
       })
+    },
+    buildChartOptions (title) {
+      let options = {
+        chart: {
+          renderTo: 'req_timeoutCount_chart_all',
+          type: 'spline',
+          zoomType: 'x',
+          panning: true,
+          panKey: 'shift'
+        },
+        credits: {
+          enabled: false
+        },
+        title: {
+          useHTML: true,
+          text: title,
+          style: {
+            font: 'normal 16px Verdana, sans-serif'
+          }
+        },
+        xAxis: {
+          type: 'datetime',
+          labels: {}
+        },
+        yAxis: {
+          title: {
+            text: ''
+          },
+          labels: {}
+        },
+        plotOptions: {
+          spline: {
+            marker: {
+              enabled: false
+            },
+            turboThreshold: 5000
+          },
+          series: {
+            cursor: 'pointer',
+            events: {}
+          }
+        },
+        tooltip: {
+          crosshairs: true,
+          shared: true
+        },
+        exporting: {
+          enabled: false
+        },
+        series: [
+        ]
+      }
+      return options
     },
     convert (data) {
       // console.info(data)
       let series = []
       data.forEach((item) => {
         let serie = {
-          name: item.metric,
+          name: item.tags.key,
           data: []
         }
         Object.keys(item.dps).forEach((key) => {
